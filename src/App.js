15dec75ace5eb05c5e21c5dd2d3321feea087341
dmerci1 <danmercia@gmail.com>
2018-01-08 23:01:07 -0500

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
-export default class App extends React.Component {
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDbD2m16aSiIvPcbZ3nRpib3lei2MqOIEU',
+      authDomain: 'pupple-4e40e.firebaseapp.com',
+      databaseURL: 'https://pupple-4e40e.firebaseio.com',
+      projectId: 'pupple-4e40e',
+      storageBucket: 'pupple-4e40e.appspot.com',
+      messagingSenderId: '1007949599264'
+    });
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This will be pupple!</Text>
+      <View>
+        <Text>Hello Pupple!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
